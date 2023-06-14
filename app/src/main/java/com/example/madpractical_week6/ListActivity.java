cecdf4407560f95c2c42a0739d3495f3dfca0a5f
Thu Ta <s10242515@connect.np.edu..sg>
2023-06-15 01:15:26 +0800

    c

--- app/src/main/java/com/example/madpractical_week6/ListActivity.java
+++ app/src/main/java/com/example/madpractical_week6/ListActivity.java
@@ -1,7 +1,11 @@
 package com.example.madpractical_week6;
 
+import static com.example.madpractical_week6.MyDBhandler.TABLE_USERS;
+
 import android.annotation.SuppressLint;
+import android.database.sqlite.SQLiteDatabase;
 import android.os.Bundle;
+import android.util.Log;
 
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.recyclerview.widget.DefaultItemAnimator;
@@ -14,35 +18,43 @@ import java.util.Random;
 
 public class ListActivity extends AppCompatActivity {
 
+
+
     @SuppressLint("MissingInflatedId")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_list);
 
-        List<User> userList = new ArrayList<>();
         MyDBhandler dbHandler = new MyDBhandler(this, null, null,1);
+        SQLiteDatabase db = dbHandler.getWritableDatabase();
 
-        for (int i = 0; i < 20; i++) {
-            User user = new User();
+        // Check if the database is empty
+        if (dbHandler.isDatabaseEmpty()) {
+            // Database is empty, populate it with 20 user records
 
-            // Randomize the name as an integer
-            int randomName = getRandomInt(0, 10000000);
-            String name = "Name: " + randomName;
-            user.setName(name);
+            for (int i = 0; i < 20; i++) {
+                User user = new User();
 
-            // Randomize the description (with integers)
-            int randomDescription = getRandomInt(0, 10000000);
-            String description = "Description: " + randomDescription;
-            user.setDescription(description);
+                // Randomize the name as an integer
+                int randomName = getRandomInt(0, 10000000);
+                String name = "Name: " + randomName;
+                user.setName(name);
 
-            // Randomize the value of Followed
-            boolean randomFollowed = getRandomInt(0, 1) == 1;
-            user.setFollowed(randomFollowed);
+                // Randomize the description (with integers)
+                int randomDescription = getRandomInt(0, 10000000);
+                String description = "Description: " + randomDescription;
+                user.setDescription(description);
 
-            userList.add(user);
-            dbHandler.addUser(user);
+                // Randomize the value of Followed
+                boolean randomFollowed = getRandomInt(0, 1) == 1;
+                user.setFollowed(randomFollowed);
+
+                dbHandler.addUser(user);
+            }
         }
+
+        List<User> userList = dbHandler.getUsers();
 
         RecyclerView recyclerView = findViewById(R.id.recyclerView);
         UserAdapter mAdapter = new UserAdapter((ArrayList<User>) userList, this);
@@ -51,7 +63,7 @@ public class ListActivity extends AppCompatActivity {
         recyclerView.setItemAnimator(new DefaultItemAnimator());
         recyclerView.setAdapter(mAdapter);
 
-
+        db.close();
     }
 
     private int getRandomInt(int min, int max) {
--- app/src/main/java/com/example/madpractical_week6/MyDBhandler.java
+++ app/src/main/java/com/example/madpractical_week6/MyDBhandler.java
@@ -1,11 +1,17 @@
 package com.example.madpractical_week6;
 
+import android.annotation.SuppressLint;
 import android.content.ContentValues;
 import android.content.Context;
+import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
 import android.database.sqlite.SQLiteOpenHelper;
+import android.util.Log;
 
 import androidx.annotation.Nullable;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class MyDBhandler extends SQLiteOpenHelper {
 
@@ -18,18 +24,27 @@ public class MyDBhandler extends SQLiteOpenHelper {
     public static final String COLUMN_FOLLOWED = "followed";
 
     public MyDBhandler(@Nullable Context context, @Nullable String name, @Nullable SQLiteDatabase.CursorFactory factory, int version) {
-        super(context, name, factory, version);
+        super(context, DATABASE_NAME, factory, DATABASE_VERSION);
+    }
+
+    public boolean isDatabaseEmpty() {
+        SQLiteDatabase db = this.getReadableDatabase();
+        String query = "SELECT 1 FROM " + TABLE_USERS + " LIMIT 1";
+        Cursor cursor = db.rawQuery(query, null);
+        boolean isEmpty = (cursor.getCount() == 0);
+        cursor.close();
+        return isEmpty;
     }
 
     @Override
     public void onCreate(SQLiteDatabase db) {
-        String createTableQuery = "CREATE TABLE " + TABLE_USERS + " (" +
-                COLUMN_ID + " INTEGER PRIMARY KEY, " +
+        String CREATE_USERS_TABLE = "CREATE TABLE " + TABLE_USERS + " (" +
+                COLUMN_ID + " INTEGER PRIMARY KEY AUTOINCREMENT, " +
                 COLUMN_NAME + " TEXT, " +
                 COLUMN_DESCRIPTION + " TEXT, " +
                 COLUMN_FOLLOWED + " INTEGER)";
-
-        db.execSQL(createTableQuery);
+        Log.v("Create", "created");
+        db.execSQL(CREATE_USERS_TABLE);
     }
 
     @Override
@@ -48,8 +63,39 @@ public class MyDBhandler extends SQLiteOpenHelper {
         values.put(COLUMN_FOLLOWED, user.getFollowed() ? 1 : 0);
 
         db.insert(TABLE_USERS, null, values);
+        Log.v("Added", "added");
         db.close();
     }
+
+    @SuppressLint("Range")
+    public List<User> getUsers() {
+        List<User> userList = new ArrayList<>();
+
+        SQLiteDatabase db = this.getReadableDatabase();
+
+        String selectQuery = "SELECT * FROM " + TABLE_USERS;
+
+        Cursor cursor = db.rawQuery(selectQuery, null);
+
+        if (cursor.moveToFirst()) {
+            do {
+                User user = new User();
+                user.setId(cursor.getInt(cursor.getColumnIndex(COLUMN_ID)));
+                user.setName(cursor.getString(cursor.getColumnIndex(COLUMN_NAME)));
+                user.setDescription(cursor.getString(cursor.getColumnIndex(COLUMN_DESCRIPTION)));
+                user.setFollowed(cursor.getInt(cursor.getColumnIndex(COLUMN_FOLLOWED)) == 1);
+
+                userList.add(user);
+            } while (cursor.moveToNext());
+        }
+
+        cursor.close();
+        db.close();
+
+        return userList;
+    }
+
+
 }
 
 
